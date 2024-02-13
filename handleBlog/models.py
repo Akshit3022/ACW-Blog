5f026010c7bfb5706f50d375d9b1ae28ecfeb8e6
Akshit3022 <akshit.chauhan@yudiz.com>
2024-02-13 18:09:59 +0530

    Added 4 models

--- handleBlog/models.py
+++ handleBlog/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
+class CustomUser(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    userName = models.CharField(max_length=100)
+    userEmail = models.EmailField()
+    userpassword = models.CharField(max_length=100)
+    userPhone = models.CharField(max_length=50)
+    userAbout = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.userName
+    
+class Blog(models.Model):
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True)
+    blog_id = models.AutoField(primary_key=True)
+    blogTitle = models.CharField(max_length=100)
+    blogImage = models.ImageField()
+    blogContent = models.TextField(max_length=10000) 
+    blogDate = models.DateTimeField()
+
+    def __str__(self):
+        return self.blogTitle
+
+class Comment(models.Model):
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True)
+    blog_id = models.ForeignKey(Blog, on_delete=models.CASCADE, null=True)
+    comment_id = models.AutoField(primary_key=True)
+    commentContent = models.TextField(max_length=300)
+    commentDate = models.DateTimeField()
+
+    def __str__(self):
+        return self.user_id
+    
+class Rating(models.Model):
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True)
+    blog_id = models.ForeignKey(Blog, on_delete=models.CASCADE, null=True)
+    rating_id = models.AutoField(primary_key=True)
+    ratingValue = models.FloatField(null=True)
+
+    def __str__(self):
+        return self.user_id
---
+++ handleBlog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('', views.login, name='login'),
+    # path('register', views.register, name='register'),
+    # path('logout', views.logout, name='logout'),
+    # path('home', views.home, name='home'),
+    # path('post', views.post, name='post'),
+    # path('post/<int:id>', views.post_detail, name='post_detail'),
+]
